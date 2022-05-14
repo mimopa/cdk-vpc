06d5f22ca4c078016569a3e41ba1ae2cb9af14ef
Jun Terauchi <Jun@MotokanoiMac.local>
2022-05-14 16:36:55 +0900

    modify

--- lib/cdk_test-stack.ts
+++ lib/cdk_test-stack.ts
@@ -1,5 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { DefaultInstanceTenancy } from 'aws-cdk-lib/aws-ec2';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkTestStack extends Stack {
@@ -12,5 +14,24 @@ export class CdkTestStack extends Stack {
     // const queue = new sqs.Queue(this, 'CdkTestQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+        /**
+     * VPC LambdaFunctionUrlsVpc
+     */
+    // const vpc = new ec2.Vpc(this, 'HandsonVpc', { cidr: '10.10.0.0/16 '});
+    const vpc = new ec2.Vpc(this, 'HandsonVpc', {
+    vpcName: '01997041-handson',
+    cidr : '10.10.0.0/16',
+    defaultInstanceTenancy: DefaultInstanceTenancy.DEFAULT,
+    enableDnsSupport: true,
+    enableDnsHostnames: true,
+    subnetConfiguration: [
+      { cidrMask: 24, name: 'Public-Subnet-1a', subnetType: ec2.SubnetType.PUBLIC },
+      { cidrMask: 24, name: 'Public-Subnet-1c', subnetType: ec2.SubnetType.PUBLIC },
+      { cidrMask: 24, name: 'Private-Subnet-1a', subnetType: ec2.SubnetType.PRIVATE_ISOLATED},
+      { cidrMask: 24, name: 'Private-Subnet-1c', subnetType: ec2.SubnetType.PRIVATE_ISOLATED},
+      // { cidrMask: 24, name: 'Private-Subnet-1c', subnetType: ec2.SubnetType.PRIVATE_WITH_NAT},
+    ]
+  });
+    
   }
 }
